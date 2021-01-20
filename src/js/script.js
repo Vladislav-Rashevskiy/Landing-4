d6bc824a303c635f4be7618f3fe0334aa3e50e03
Vladislav <vladislav.rashevskiy@gmail.com>
2021-01-20 18:08:28 +0300

    services

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded",() => {
     const menu = document.querySelector(".header-navbar__menu");
     const burgerButton = document.querySelector(".header-navbar__btn");
+    const tabsButtons = document.querySelectorAll(".services-tabs__btn");
+    const tabsItems= document.querySelectorAll(".services-tabs__items");
+
+    tabsButtons.forEach((element)=>{
+        element.addEventListener("click",()=>{
+            let currentBtn=element;
+            let tabId = currentBtn.getAttribute("data-tabs");
+            let currentId = document.querySelector(tabId);
+
+            tabsButtons.forEach((element)=>{
+                element.classList.remove("active");
+            });
+            tabsItems.forEach((element)=>{
+                element.classList.remove("active");
+            });
+            currentBtn.classList.add("active");
+            currentId.classList.add("active");
+        });
+    });
 
     burgerButton.addEventListener("click",() => {
        if(menu.classList.contains("active")){
@@ -10,6 +29,8 @@ document.addEventListener("DOMContentLoaded",() => {
         menu.classList.add("active")
         burgerButton.classList.add("active")
        }
-    })
+    });
+  
+
 });
 
