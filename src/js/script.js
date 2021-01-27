c981d22c9968e09e3c5cc62c2e1c6b195c6990aa
Vladislav <vladislav.rashevskiy@gmail.com>
2021-01-27 20:31:39 +0300

    adapt part2

--- src/js/script.js
+++ src/js/script.js
@@ -30,15 +30,45 @@ document.addEventListener("DOMContentLoaded", () => {
       burgerButton.classList.add("active");
     }
   });
+  $(".header-button__btn").click(() => {
+    $(".form-container").slideToggle();
+    $(".form-container").css({ display: "flex", "justify-content": "center" });
+  });
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: "<button class='prev'><</button>",
+    nextArrow: "<button class='next'>></button>",
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $(document).on("click", ".header-navbar__links", function (event) {
+    event.preventDefault();
+    let href = $(this).attr("href");
+    console.log($(this));
+    $("html, body").animate(
+      {
+        scrollTop: $(href).offset().top,
+      },
+      2000
+    );
+  });
 });
-$('.slider').slick({
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  prevArrow: "<button class='prev'><</button>",
-  nextArrow: "<button class='next'>></button>",
-  dots:true,
-});
-$(".header-button__btn").click(()=>{
-  $(".header-form").toggle();
-})
